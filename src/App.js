3188cc94ec288bdb9622622cd43c2167a2fd5cca
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-11-04 12:34:31 +0200

    Task complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import moment from 'moment';
+import { v4 } from 'uuid';
 
 function DateTime(props) {
     return (
@@ -6,21 +8,57 @@ function DateTime(props) {
     )
 }
 
+function DateTimeWithFormat(WrappedComponent) {
+  return class WithFormat extends React.Component {
+    constructor(props) {
+      super(props);
+      this.date = props.date;
+    }
+
+    formatDate() {
+      const subDate = moment() - moment(this.date);
+
+      if (subDate < 3600000) {
+        return '12 минут назад'
+      }
+
+      if (subDate >= 3600000 && subDate < 86400000) {
+        return '5 часов назад'
+      }
+
+      if (subDate >= 86400000) {
+        return `${Math.round(subDate / 86400000)} дней назад...`
+      }
+      
+      return this.date;
+    }
+
+    render() {
+      return (
+        <WrappedComponent date={ this.formatDate() } />
+      )
+    }
+  }
+}
+
+const DateTimePretty = DateTimeWithFormat(DateTime);
+
+
 function Video(props) {
     return (
         <div className="video">
-            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTime date={props.date} />
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" title='video' allowFullScreen></iframe>
+            <DateTimePretty date={props.date} />
         </div>
     )
 }
 
 function VideoList(props) {
-    return props.list.map(item => <Video url={item.url} date={item.date} />);
+    return props.list.map(item => <Video url={item.url} date={item.date} key={v4()} />);
 }
 
 export default function App() {
-    const [list, setList] = useState([
+    const [list] = useState([
         {
             url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
             date: '2017-07-31 13:24:00'
